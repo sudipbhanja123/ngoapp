3b00f1f1942980f21e524a0781098cacf54745ee
sudipbhanja123 <sudipbhanja5g@gmail.com>
2025-03-27 13:33:05 +0530

    updated gallery

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,17 @@ const Contact = () => {
         <meta name="description" content="Get in touch with Caring Crowd." />
       </Helmet>
       <h2 className="text-3xl text-center font-bold">CONTACT US</h2>
+      {/* <div class="g_map mt-8">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722.483625506544!2d81.0342308!3d21.0932766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a2945ab2194c349%3A0xcebf0abdd0677323!2sGaushala%20Rd%2C%20Rajnandgaon%2C%20Chhattisgarh%20491441!5e0!3m2!1sen!2sin!4v1690356247937!5m2!1sen!2sin"
+          width="100%"
+          height="600"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div> */}
       <p>
         We want to hear from you! Drop us a note and someone from our team will
         get back to you.
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,23 +1,71 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import image1 from "../assets/images/images (1).jpg";
+import image2 from "../assets/images/images.jpg";
+import pro1 from "../assets/images/pro1.svg";
 const Gallery = () => {
   return (
-    <div className="gallery bg-blue-100 py-12">
-      <Helmet>
-        <title>Caring Crowd | Gallery</title>
-        <meta
-          name="description"
-          content="View our gallery of images showcasing our work."
-        />
-      </Helmet>
-      <h2 className="text-3xl text-center font-bold">What We Do?</h2>
-      <div className="image-container mt-8">
-        <img src={image1} alt="Gallery Image 1" />
-        <img src="images/image2.jpg" alt="Gallery Image 2" />
-        {/* Add more images as needed */}
+    <>
+      <div class="main_about bg-blue-100 py-12 md:py-16">
+        <div class="main_content_about text-center md:text-left">
+          <h2 class="text-3xl font-bold">What We Do?</h2>
+        </div>
       </div>
-    </div>
+
+      <div className="selfie flex py-5 gap-6 px-5">
+        <img src={image1} alt="Gallery Image 1" />
+        <img src={image2} alt="Gallery Image 2" />
+      </div>
+      {/* How We Help Section */}
+      <div className="product bg-blue-100 py-12 md:py-16">
+        <h2 className="text-3xl text-center font-bold">
+          <span style={{ color: "black" }}>HOW WE HELP</span>
+        </h2>
+        <div className="product_container mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="item">
+            <div className="item_img">
+              <img className="pro" src={pro1} alt="Pure Food & Water" />
+            </div>
+            <div className="item_content mt-4 text-center md:text-left">
+              <h3 className="text-xl font-semibold">Pure Food & Water</h3>
+              <p className="mt-4">
+                We supply needy children with basic necessities like pure food
+                and water
+              </p>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="item_img">
+              <img
+                className="pro"
+                src="images/pro2.svg"
+                alt="Health and Medicine"
+              />
+            </div>
+            <div className="item_content mt-4 text-center md:text-left">
+              <h3 className="text-xl font-semibold">Health and Medicine</h3>
+              <p className="mt-4">
+                Health being the foremost priority, we aim at giving children
+                every medical support
+              </p>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="item_img">
+              <img className="pro" src="images/pro3.svg" alt="Education" />
+            </div>
+            <div className="item_content mt-4 text-center md:text-left">
+              <h3 className="text-xl font-semibold">Education</h3>
+              <p className="mt-4">
+                We provide education facilities to children all over the world
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
